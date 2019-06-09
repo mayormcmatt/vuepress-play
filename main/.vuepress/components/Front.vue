7dd9ed28d1ceab63ce2d869d04647b6a2d992740
Matthew Lopez <matthew.is.riding@gmail.com>
2019-06-09 13:32:45 -0700

    Added component and SCSS support

---
+++ main/.vuepress/components/Front.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>I am Front component</h2>
+</template>
+
+<style lang="scss" scoped>
+    h2 {
+        color: red;
+    }
+</style>
--- main/.vuepress/config.js
+++ main/.vuepress/config.js
@@ -1,4 +1,15 @@
 module.exports = {
     title: 'Playing with Vuepress',
-    description: 'Just futzing around'
+    description: 'Just futzing around',
+    base: '/vuepress-play/',
+    scss: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    }
 }
